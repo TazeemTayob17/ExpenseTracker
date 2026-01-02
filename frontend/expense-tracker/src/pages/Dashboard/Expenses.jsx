ffc52168c98f532b3bd443ae4c1ce043cca8a01a
TazeemTayob17 <tazeemtayob17@gmail.com>
2026-01-02 11:44:37 +0200

    Expense List Section

--- frontend/expense-tracker/src/pages/Dashboard/Expenses.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Expenses.jsx
@@ -7,6 +7,8 @@ import axiosInstance from "../../utils/axiosInstance";
 import ExpenseOverview from "../../components/Expense/ExpenseOverview";
 import Modal from "../../components/Modal";
 import AddExpenseForm from "../../components/Expense/AddExpenseForm";
+import ExpenseList from "../../components/Expense/ExpenseList";
+import DeleteAlert from "../../components/DeleteAlert";
 
 const Expenses = () => {
   useUserAuth();
@@ -131,6 +133,17 @@ const Expenses = () => {
           title="Add Expense"
         >
           <AddExpenseForm onAddExpense={handleAddExpense} />
+        </Modal>
+
+        <Modal
+          isOpen={openDeleteAlert.show}
+          onClose={() => setOpenDeleteAlert({ show: false, data: null })}
+          title="Delete Expense"
+        >
+          <DeleteAlert
+            content="Are you sure you want to delete this expense entry?"
+            onDelete={() => deleteExpense(openDeleteAlert.data)}
+          />
         </Modal>
       </div>
     </DashboardLayout>
